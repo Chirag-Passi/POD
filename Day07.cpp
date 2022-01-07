8de0d5d862b75722b4d6ef1f0c4394f2a3fc02df
Chirag <chiragpassi1999@gmail.com>
2022-01-07 10:15:52 +0530

    Daoy 07 Problem

---
+++ Day07.cpp
@@ -0,0 +1,24 @@
+class Solution
+{
+public:
+    bool isPalindrome(int x)
+    {
+
+        if (x < 0) // if num 0 or negative return false
+            return false;
+
+        long long reverse = 0; // long long for --> 2,147,483,648
+        int temp = x;
+
+        while (temp > 0)
+        {
+            reverse = reverse * 10 + temp % 10;
+            temp /= 10;
+        }
+
+        if (x == reverse) // Palindrome Condition
+            return true;
+        else
+            return false;
+    }
+};
