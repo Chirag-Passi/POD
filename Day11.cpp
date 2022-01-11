0199b7deafb1b7988c27d3db157a1c17cfc867d3
CHIRAG PASSI <96006412+Chirag-Passi@users.noreply.github.com>
2022-01-11 22:26:34 +0530

    Day 11 Problem

---
+++ Day11.cpp
@@ -0,0 +1,27 @@
+/* LeetCode : 744 ,  Find Smallest Letter Greater Than Target */
+
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        int start = 0;
+        int end = letters.size() - 1;
+        char ans = letters[0];
+        
+        while(start <= end){
+            int mid = start + (end - start)/2;
+            
+            if(letters[mid] > target){
+                ans = letters[mid];
+                end = mid - 1;
+            }
+                
+            else{
+                start = mid + 1;
+            }
+        }
+        return ans ;
+    }
+};
+
+/*  Time Complexity: O(logN)
+    Space Complexity: O(1)  */
