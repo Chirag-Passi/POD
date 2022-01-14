c95a25db11ed16f71554d6c4083ec67742c43336
CHIRAG PASSI <96006412+Chirag-Passi@users.noreply.github.com>
2022-01-14 09:46:25 +0530

    DAy 14 Problem

---
+++ Day14.cpp
@@ -0,0 +1,21 @@
+/* LeetCode : 69. Sqrt(x) */
+class Solution {
+public:
+    int mySqrt(int x) {
+        int s = 0  , e = x;
+        int ans = 0;
+        while(s<=e){
+            long long mid = s + (e-s)/2;
+            
+            if(mid*mid <= x){
+                ans = mid;
+                s = mid+1;
+            }
+            
+            else{
+                e = mid-1;
+            }
+        }
+        return ans;
+    }
+};
